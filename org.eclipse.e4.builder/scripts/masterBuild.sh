6fee6c5dd60d5f022eeaaa06c89b13a4c8ff043c
Paul Webster <pwebster>
2010-05-10 17:37:10 +0000

    Update to the latest basebuilder, see how we stack up against 3.6 M7

--- org.eclipse.e4.builder/scripts/masterBuild.sh
+++ org.eclipse.e4.builder/scripts/masterBuild.sh
@@ -32,7 +32,7 @@ realBuildProperties () {
 # available builds
     #basebuilderBranch=$( grep v2009 /cvsroot/eclipse/org.eclipse.releng.basebuilder/about.html,v | head -1 | cut -f1 -d: | tr -d "[:blank:]" )
     #eclipseIBuild=$( ls -d /home/data/httpd/download.eclipse.org/eclipse/downloads/drops/I*/eclipse-SDK-I*-linux-gtk${archProp}.tar.gz | tail -1 | cut -d/ -f9 )
-    basebuilderBranch=I20091117-1545
+    basebuilderBranch=v20100509
 }
 
 
@@ -50,7 +50,7 @@ testBuildProperties () {
     buildtime=$( date +%H%M )
 
     projRoot=':pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse'
-    basebuilderBranch=I20091117-1545
+    basebuilderBranch=v20100509
 }
 
 commonProperties () {
