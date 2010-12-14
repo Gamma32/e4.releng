a95d82905bbbdc468434dfa25144f7284c51fa8b
Andrew Niefer <aniefer>
2010-12-14 19:15:22 +0000

    bug 331237 build 4.1 first

--- org.eclipse.e4.builder/scripts/masterBuild.sh
+++ org.eclipse.e4.builder/scripts/masterBuild.sh
@@ -198,8 +198,8 @@ runSDKBuild () {
 	"   
     echo $cmd
     $cmd
-	/bin/bash ${builderDir}/scripts/sync.sh
-	/bin/bash ${builderDir}/scripts/publish.sh
+	#/bin/bash ${builderDir}/scripts/sync.sh
+	#/bin/bash ${builderDir}/scripts/publish.sh
 }
 
 
