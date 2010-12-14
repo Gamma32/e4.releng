28eb8944f0265410a5ba3f388a3764b90684dfdf
Andrew Niefer <aniefer>
2010-12-14 19:29:17 +0000

    bug 331237 build 4.1 first

--- org.eclipse.e4.builder/scripts/bootstrap.sh
+++ org.eclipse.e4.builder/scripts/bootstrap.sh
@@ -6,13 +6,13 @@ supportDir=$writableBuildRoot/build/e4
 
 cd $supportDir
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update -r R4_1_SDK_LONGBUILD \
-  -d org.eclipse.e4.builder_HEAD
+  -d org.eclipse.e4.builder_R4_1_SDK_LONGBUILD
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update -r R4_1_SDK_LONGBUILD \
-  -d org.eclipse.e4.sdk
+  -d org.eclipse.e4.sdk_R4_1_SDK_LONGBUILD
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
   -d org.eclipse.releng.eclipsebuilder
 
-cp org.eclipse.e4.builder_HEAD/scripts/masterBuild.sh $writableBuildRoot
+cp org.eclipse.e4.builder_R4_1_SDK_LONGBUILD/scripts/masterBuild.sh $writableBuildRoot
 
 cd $writableBuildRoot
 /bin/bash -l $writableBuildRoot/masterBuild.sh >$writableBuildRoot/logs/current.log 2>&1
