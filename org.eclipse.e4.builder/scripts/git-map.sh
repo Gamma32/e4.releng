a69210cc8fbd5e83a03ca7ebc058685a279b1c98
Andrew Niefer <aniefer>
2011-01-21 20:44:58 +0000

    bug 331146 - Git tagging must deal with IDs

--- org.eclipse.e4.builder/scripts/git-map.sh
+++ org.eclipse.e4.builder/scripts/git-map.sh
@@ -20,19 +20,28 @@ LATEST_SUBMISSION=""
 for proj in $PROJECTS; do
     echo Processing $proj
     PROJ_LINE=$( grep "@${proj}=" ${MAPFILE} )
+    projId=$proj
+    
+    if [ -z "$PROJ_LINE" ]; then
+    	PROJ_LINE=$( grep "path=${BASEDIR}/${proj}" ${MAPFILE} )
+    	projId=$( echo $PROJ_LINE | sed -e 's:^\(plugin\|feature\|fragment\|bundle\)@\([-._A-Za-z0-9]*\)=.*$:\2:' )
+    fi
+    
     if [ ! -z "$PROJ_LINE" ]; then
 	LAST_COMMIT=$( git rev-list HEAD -- $BASEDIR/$proj | head -1 )
 	LAST_SUBMISSION=$( echo $PROJ_LINE | sed -e 's/^.*tag=//' | sed -e 's/,.*$//')
 	if [ "$LATEST_SUBMISSION" \< "$LAST_SUBMISSION" ]; then
 	    LATEST_SUBMISSION=$LAST_SUBMISSION
 	fi
-	echo commit: $LAST_COMMIT
-	echo map: $LAST_SUBMISSION
-	echo latest: $LATEST_SUBMISSION
+	
+	LAST_COMMIT_DATE=$( git log --pretty=format:"%H %ad" --date=iso | grep $LAST_COMMIT | sed -e 's:[a-f0-9]* \(.*\)$:\1:' )
+	echo Last commit was at $LAST_COMMIT_DATE
+	echo Map file entry is $LAST_SUBMISSION
+	
 	if ! ( git tag --contains $LAST_COMMIT | grep $LAST_SUBMISSION >/dev/null ); then
 	    echo Needs to move from $LAST_SUBMISSION to $BUILD_TAG
 	    echo ${proj} >>$CHPROJ
-	    sed "s/@${proj}=GIT,tag=$LAST_SUBMISSION/@${proj}=GIT,tag=$BUILD_TAG/g" $MAPFILE >/tmp/t1.txt
+	    sed "s/@${projId}=GIT,tag=$LAST_SUBMISSION/@${projId}=GIT,tag=$BUILD_TAG/g" $MAPFILE >/tmp/t1.txt
 	    mv /tmp/t1.txt $MAPFILE
 	fi
     fi
