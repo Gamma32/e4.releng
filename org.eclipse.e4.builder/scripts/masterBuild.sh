6978961c884f577a66949d9a6afc7c8fbb8bc297
Andrew Niefer <aniefer>
2011-03-02 19:55:56 +0000

    update to basebuilder v20110302

--- org.eclipse.e4.builder/scripts/masterBuild.sh
+++ org.eclipse.e4.builder/scripts/masterBuild.sh
@@ -34,7 +34,7 @@ realBuildProperties () {
 # available builds
     #basebuilderBranch=$( grep v2009 /cvsroot/eclipse/org.eclipse.releng.basebuilder/about.html,v | head -1 | cut -f1 -d: | tr -d "[:blank:]" )
     #eclipseIBuild=$( ls -d /home/data/httpd/download.eclipse.org/eclipse/downloads/drops/I*/eclipse-SDK-I*-linux-gtk${archProp}.tar.gz | tail -1 | cut -d/ -f9 )
-    basebuilderBranch=v20101019
+    basebuilderBranch=v20110302
 }
 
 
