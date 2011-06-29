6717986ca473efea79441af894f0fb35882f4204
Andrew Niefer <aniefer>
2011-06-29 20:37:45 +0000

    add branching to bootstrap script

--- org.eclipse.e4.builder/scripts/bootstrap.sh
+++ org.eclipse.e4.builder/scripts/bootstrap.sh
@@ -3,16 +3,26 @@
 
 writableBuildRoot=/shared/eclipse/e4
 supportDir=$writableBuildRoot/build/e4
+relengBranch=HEAD
+while [ $# -gt 0 ]
+do
+	case "$1" in		
+		"-branch")
+			relengBranch="$2"; shift;;
+		 *) break;;	 # terminate while loop
+	esac
+	shift
+done	
 
 cd $supportDir
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
-  -d org.eclipse.e4.builder_HEAD
+  -d org.eclipse.e4.builder_$relengBranch
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
-  -d org.eclipse.e4.sdk_HEAD
+  -d org.eclipse.e4.sdk_$relengBranch
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
   -d org.eclipse.releng.eclipsebuilder
 
-cp org.eclipse.e4.builder_HEAD/scripts/masterBuild.sh $writableBuildRoot
+cp org.eclipse.e4.builder_$relengBranch/scripts/masterBuild.sh $writableBuildRoot
 
 cd $writableBuildRoot
 /bin/bash -l $writableBuildRoot/masterBuild.sh >$writableBuildRoot/logs/current.log 2>&1
