509a04a902f6f130173fb19958c103046ca80599
Paul Webster <pwebster>
2011-07-11 11:18:52 +0000

    A better CVS map to git map conversion script

--- org.eclipse.e4.builder/scripts/git-gen-map.sh
+++ org.eclipse.e4.builder/scripts/git-gen-map.sh
@@ -1,30 +1,53 @@
 #!/bin/bash
 #
-# USAGE: /bin/bash git-gen-map.sh REPO-URL TAG <relative-directory-path-with-plugins>+
-# ex: /bin/bash /opt/pwebster/workspaces/e4-search/releng/org.eclipse.e4.builder/scripts/git-gen-map.sh git://git.eclipse.org/gitroot/e4/org.eclipse.e4.search.git v20101124-0800 bundles tests features
-#
+# this is a much better script for converting CVS maps to git maps.
+# you need to have org.eclipse.releng checked out in $WS
+# and you need to list the repos you want to convert to.  For each
+# repo, git://git.eclipse.org/gitroot/platform/eclipse.platform.ui.git,
+# the clone must exist at $ROOT/eclipse.platform.ui
 
-REPO=$1
-shift
-TAG=$1
-shift
 
-gen_line () {
-DIR=$1
-PLUGIN=$2
-START=plugin
-if [ "$DIR" = "features" ]; then
-	START=feature
-fi
-echo "${START}@${PLUGIN}=GIT,tag=${TAG},repo=${REPO},path=${DIR}/${PLUGIN}"
+ROOT=/opt/pwebster/git/eclipse
+WS=/opt/pwebster/workspaces/gitMigration
+
+REPOS='
+git://git.eclipse.org/gitroot/platform/eclipse.platform.runtime.git 
+git://git.eclipse.org/gitroot/platform/eclipse.platform.ui.git 
+git://git.eclipse.org/gitroot/e4/org.eclipse.e4.tools.git 
+git://git.eclipse.org/gitroot/e4/org.eclipse.e4.ui.git
+'
+
+cd $ROOT
+
+update_map () {
+	REPO=$1
+	REPO_DIR=$( basename $REPO .git )
+	M=$2
+	ID=$3
+	MAP=$4
+	if [ ! -z "$5" ]; then
+		echo Extra map $5
+	fi
+	
+	REPO_PATH=$( echo $REPO | sed 's/\//\\\//g' )
+	M_PATH=$( echo $M | sed 's/^[^/]*\///g' | sed 's/\//\\\//g' )
+	echo sed "'s/@${ID}=\([^,]*\),.*$/@${ID}=GIT,tag=\1,repo=${REPO_PATH},path=${M_PATH}/g' $MAP >t1.txt ; mv t1.txt $MAP "
 }
 
 
-for DIR in "$@"; do
-	for f in ${DIR}/*; do
-		FN=$( basename $f )
-		gen_line $DIR $FN
+# find the map files
+for REPO in $REPOS; do
+	REPO_DIR=$( basename $REPO .git )
+	MODULES=$( ls -d $REPO_DIR/*/* )
+	for M in $MODULES; do
+		ID=$( basename $M )
+		MAP=$( find $WS/org.eclipse.releng/maps -name "*.map" -exec grep -l "@${ID}=" {} \; )
+		if [ ! -z "$MAP" ]; then
+			update_map $REPO $M $ID $MAP
+		fi
+		#MAP=$( find $WS/releng -name "*.map" -exec grep -l "@${ID}=" {} \; )
+		#if [ ! -z "$MAP" ]; then
+		#	update_map $REPO $M $ID $MAP
+		#fi
 	done
-	echo ""
-	echo ""
 done
