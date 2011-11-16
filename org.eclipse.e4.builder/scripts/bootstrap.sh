cc8a089d384bce006ced2da456fb610651fd9683
Paul Webster <pwebster@ca.ibm.com>
2011-11-16 12:41:10 -0500

    Update the bootstrap script to reflect the build server

--- org.eclipse.e4.builder/scripts/bootstrap.sh
+++ org.eclipse.e4.builder/scripts/bootstrap.sh
@@ -3,16 +3,27 @@
 
 writableBuildRoot=/shared/eclipse/e4
 supportDir=$writableBuildRoot/build/e4
+relengBranch=HEAD
+while [ $# -gt 0 ]
+do
+        case "$1" in
+                "-branch")
+                        relengBranch="$2"; shift;;
+                 *) break;;      # terminate while loop
+        esac
+        shift
+done
+
 
 cd $supportDir
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
-  -d org.eclipse.e4.builder_HEAD
+  -d org.eclipse.e4.builder_$relengBranch
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
-  -d org.eclipse.e4.sdk_HEAD
+  -d org.eclipse.e4.sdk_$relengBranch
 cvs -d :pserver:anonymous@dev.eclipse.org:/cvsroot/eclipse update \
-  -d org.eclipse.releng.eclipsebuilder
+  -d org.eclipse.releng.eclipsebuilder_$relengBranch
 
-cp org.eclipse.e4.builder_HEAD/scripts/masterBuild.sh $writableBuildRoot
+cp org.eclipse.e4.builder_$relengBranch/scripts/masterBuild.sh $writableBuildRoot
 
 cd $writableBuildRoot
 /bin/bash -l $writableBuildRoot/masterBuild.sh >$writableBuildRoot/logs/current.log 2>&1
