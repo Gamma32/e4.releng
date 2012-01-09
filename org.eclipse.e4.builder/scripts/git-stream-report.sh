aa39368bbcaaab9b20e84bf5b8d1530a6247da03
Paul Webster <pwebster@ca.ibm.com>
2012-01-09 09:21:51 -0500

    Bug 359616 - Reorganize split stream development to reduce error prone synching
    Update to report commit hashes as well as subjects

--- org.eclipse.e4.builder/scripts/git-stream-report.sh
+++ org.eclipse.e4.builder/scripts/git-stream-report.sh
@@ -8,15 +8,16 @@ BRANCH_POINT=pre_R4_HEAD_merge
 EXCLUDE_DIRS=$REPO/git_report/git_stream_exclude_dirs.txt
 INCLUDE_DIRS=$REPO/git_report/git_stream_include_dirs.txt
 EXCLUDE_COMMITS=$REPO/git_report/git_stream_exclude_commits.txt
-MISSING_COMMITS=/tmp/missing_commits.txt
+TMP_DIR=/tmp/report_$$
+MISSING_COMMITS=$TMP_DIR/missing_commits.txt
 
 STREAM_42_DIR=$( dirname $STREAM_42 )
 if [ . != $STREAM_42_DIR ]; then
-	mkdir -p /tmp/${STREAM_42_DIR}
+	mkdir -p $TMP_DIR/${STREAM_42_DIR}
 fi
 STREAM_38_DIR=$( dirname $STREAM_38 )
 if [ . != $STREAM_38_DIR ]; then
-	mkdir -p /tmp/${STREAM_38_DIR}
+	mkdir -p $TMP_DIR/${STREAM_38_DIR}
 fi
 
 
@@ -24,24 +25,24 @@ ORIGINAL_DIRS=$( find * -name .project | sed 's/\/.project$//g' | sort -u )
 
 compare_proj () {
 DIR="$1"
-rm -f /tmp/${STREAM_42}.txt /tmp/${STREAM_38}.txt
+rm -f $TMP_DIR/${STREAM_42}.txt $TMP_DIR/${STREAM_38}.txt
 if grep "^$DIR$" $EXCLUDE_DIRS >/dev/null; then
 	return
 fi
 
 git log --format="%s::%H::" ${BRANCH_POINT}..${STREAM_42} \
-  -- "$DIR" | grep -v -f  $EXCLUDE_COMMITS  >/tmp/${STREAM_42}.txt
+  -- "$DIR" | grep -v -f  $EXCLUDE_COMMITS  >$TMP_DIR/${STREAM_42}.txt
 git log --format="%s::%H::" ${BRANCH_POINT}..${STREAM_38} \
-  -- "$DIR" | grep -v -f  $EXCLUDE_COMMITS  >/tmp/${STREAM_38}.txt
+  -- "$DIR" | grep -v -f  $EXCLUDE_COMMITS  >$TMP_DIR/${STREAM_38}.txt
 
 
 OLD_IFS="$IFS"
 IFS=$'\n'
 
-for line in $( cat /tmp/${STREAM_42}.txt ); do
+for line in $( cat $TMP_DIR/${STREAM_42}.txt ); do
 	SUBJECT=$( echo "$line" | sed 's/::[a-z0-9]*::$//g' | tr "\[\]" ".." )
 	COMMIT=$( echo "$line" | sed 's/^.*::\([a-z0-9]*\)::$/\1/g' )
-	if ! grep "^${SUBJECT}::" /tmp/${STREAM_38}.txt >/dev/null; then
+	if ! grep "^${SUBJECT}::" $TMP_DIR/${STREAM_38}.txt >/dev/null; then
 		echo "${line}${DIR}" >>${MISSING_COMMITS}
 	fi
 done
@@ -51,6 +52,9 @@ IFS="$OLD_IFS"
 }
 
 rm -f ${MISSING_COMMITS}
+echo "Cherry-pick: $STREAM_42 commits missing from $STREAM_38" >>${MISSING_COMMITS}
+echo >>${MISSING_COMMITS}
+
 
 #DIR=bundles/org.eclipse.ui.ide
 for DIR in $ORIGINAL_DIRS; do
