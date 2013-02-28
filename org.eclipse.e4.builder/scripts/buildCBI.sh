c6c5ad5dc7da9ad614cff8279d97216aefa7b357
Paul Webster <pwebster@ca.ibm.com>
2013-02-28 12:32:04 -0500

    Bug 401795 - [releng] create an e4 repo for databinding
    Add them to the build

--- org.eclipse.e4.builder/scripts/buildCBI.sh
+++ org.eclipse.e4.builder/scripts/buildCBI.sh
@@ -145,6 +145,7 @@ e4_tools=file:///gitroot/e4/org.eclipse.e4.tools.git
 e4_search=file:///gitroot/e4/org.eclipse.e4.search.git
 e4_lang=file:///gitroot/e4/org.eclipse.e4.languages.git
 e4_resources=file:///gitroot/e4/org.eclipse.e4.resources.git
+e4_databinding=file:///gitroot/e4/org.eclipse.e4.databinding.git
 
 #git clone file:///gitroot/cbi/org.eclipse.cbi.maven.plugins.git
 git clone $e4_releng
@@ -152,6 +153,7 @@ git clone $e4_tools
 git clone $e4_search
 git clone $e4_lang
 git clone $e4_resources
+git clone $e4_databinding
 
 # tag first
 
@@ -181,6 +183,11 @@ git tag $buildTag
 git push origin $buildTag
 popd
 
+pushd org.eclipse.e4.databinding
+git tag $buildTag
+git push origin $buildTag
+popd
+
 /bin/bash \
 org.eclipse.e4.releng/org.eclipse.e4.builder/scripts/git-submission.sh \
 $(pwd) \
@@ -191,6 +198,8 @@ $oldBuildTag $buildTag \
 $( echo $e4_lang | sed 's!file:///!git://git.eclipse.org/!g' ) \
 $oldBuildTag $buildTag \
 $( echo $e4_resources | sed 's!file:///!git://git.eclipse.org/!g' ) \
+$oldBuildTag $buildTag \
+$( echo $e4_databinding | sed 's!file:///!git://git.eclipse.org/!g' ) \
 $oldBuildTag $buildTag >submission_report.txt 2>&1
 
 mailx -s "$e4Stream Build: $buildTag submission" e4-dev@eclipse.org <submission_report.txt
@@ -241,6 +250,15 @@ $mavenBREE \
 popd
 
 pushd org.eclipse.e4.resources
+mvn $mavenVerbose \
+clean install \
+$mavenSign \
+$mavenBREE \
+-Dmaven.test.skip=true \
+-Dmaven.repo.local=$localMavenRepo
+popd
+
+pushd org.eclipse.e4.databinding
 mvn $mavenVerbose \
 clean install \
 $mavenSign \
